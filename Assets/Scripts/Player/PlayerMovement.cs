0c05ed95bbace9b625ed30ab7d037d254368e083
Aezakmi <andrearamadhan123@gmail.com>
2025-01-16 23:51:38 +0700

    full set up control

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,43 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 4;
 
+    private NavMeshAgent agent;
+
     private Vector3 targetPosition;
     private bool isMoveing = false;
 
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+    }
     private void Update()
     {
+        agent.speed = speed;
+
         if (Input.GetMouseButtonDown(0))
         {
             SetTargetPosition();
-        }
-
-        if (isMoveing)
-        {
-            Move();
         }
     }
 
     void SetTargetPosition()
     {
         targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        targetPosition.z = transform.position.z;
+
+        agent.SetDestination(targetPosition);
 
         isMoveing = true;
-    }
-
-    void Move()
-    {
-        //transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPosition);
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-        if (transform.position == targetPosition)
-        {
-            isMoveing = false;
-        }
     }
 }
 
